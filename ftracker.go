4b72bb62269515cb2f2cac6059d4169d66d7349a
Khudodatov Vitalii <hudodatov.vitaly@yandex.ru>
2024-05-23 21:31:49 +0300

    ftracker ref2

--- ftracker.go
+++ ftracker.go
@@ -106,7 +106,7 @@ const (
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
 	// ((0.035 * ВесСпортсменаВКг + (СредняяСкоростьВМетрахВСекунду**2 / РостВМетрах) * 0.029 * ВесСпортсменаВКг) * ВремяТренировкиВЧасах * minInH)
-	calories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(kmhInMsec*meanSpeed(action, duration), 2)/height)*
+	calories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(kmhInMsec*meanSpeed(action, duration), 2)/height/cmInM)*
 		walkingSpeedHeightMultiplier*weight) * duration * minInH)
 
 	return calories
