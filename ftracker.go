618b1d7ef88ea67d9e065df748a0f0c781a06439
Khudodatov Vitalii <hudodatov.vitaly@yandex.ru>
2024-05-23 21:14:05 +0300

    ftracker ref1

--- ftracker.go
+++ ftracker.go
@@ -84,7 +84,7 @@ func RunningSpentCalories(action int, weight, duration float64) float64 {
 	// ваш код здесь
 	// ((18 * СредняяСкоростьВКм/ч * 1.79) * ВесСпортсменаВКг / mInKM * ВремяТренировкиВЧасах * minInH)
 	calories := ((runningCaloriesMeanSpeedMultiplier * meanSpeed(action, duration) * runningCaloriesMeanSpeedShift) *
-		weight / mInKm * duration * minInH)
+		weight / duration * minInH)
 
 	return calories
 }
